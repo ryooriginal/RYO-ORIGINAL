7b03936974ff8aee40a533cf86078293265084a9
ryooriginal <48225684+ryooriginal@users.noreply.github.com>
2019-03-13 19:43:15 +0200

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -787,6 +787,7 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
 		return (difficulty_type)480000000;
 	if(m_nettype == MAINNET && height >= 150000 && height <= (150000 + common_config::DIFFICULTY_BLOCKS_COUNT_V3))
 		return (difficulty_type)480000;
+	
 		size_t block_count;
 	if(check_hard_fork_feature(FORK_V3_DIFFICULTY))
 		block_count = common_config::DIFFICULTY_BLOCKS_COUNT_V3;
